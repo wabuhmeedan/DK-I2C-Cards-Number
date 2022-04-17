0dbd669e0ea15533ab0f16764cb72f6d9bf81e91
wabuhmeedan <abuhmeedan@gmail.com>
2022-04-17 09:32:21 +0300

    test

--- apigee-cicd-master/test/unit/test.js
+++ apigee-cicd-master/test/unit/test.js
@@ -3,53 +3,35 @@
 var expect = require('expect.js');
 var sinon = require('sinon');
 var rewire = require('rewire');
-var app = rewire('../../NoTargetProxy/apiproxy/resources/jsc/DataMasking.js');	//Target javascript file to be tested
 
 var fakeContext = {							//declaring fake contexts
-  getVariable: function(s) {},
-  setVariable: function(s) {}
+  getVariable: function (s) { },
+  setVariable: function (s) { }
+};
+var fakeContent = {
+  getVariable: function (s) {
+  },
+  setVariable: function (s) {
+  }
 };
 
+global.print = function print(input) {
+  console.log(input);
+};
 var contextGetVariableMethod;
 var contextSetVariableMethod;
-																		//stubbing all Apigee objects and the methods we need
+//stubbing all Apigee objects and the methods we need
 beforeEach(function () {												//This method will execute before every it() method in the test
-  contextGetVariableMethod = sinon.stub(fakeContext, 'getVariable');	
-  contextSetVariableMethod = sinon.stub(fakeContext, 'setVariable');	
+  contextGetVariableMethod = sinon.stub(fakeContext, 'getVariable');
+  contextSetVariableMethod = sinon.stub(fakeContext, 'setVariable');
+
+  global.context = fakeContent;
+  global.context.getVariable = function (name) {
+    contextGetVariableMethod.withArgs(name);
+  };
 });
 
-afterEach(function() {						//restore all stubbed methods back to their original implementation
+afterEach(function () {						//restore all stubbed methods back to their original implementation
   contextGetVariableMethod.restore();
   contextSetVariableMethod.restore();
-});
-
-describe('feature: check & set Verb', function() {
-
-  it('should have GET', function() {
-    contextGetVariableMethod.withArgs('request.verb').returns("GET");
-    app.__set__('context', fakeContext);								//Setting Fake Contexts
-
-    var checkVerb = app.__get__('checkVerb');
-    expect(checkVerb()).to.equal("GET");								//Final Assertion on mocked function response
-  });
-
-  it('should set GET', function() {
-    contextSetVariableMethod.withArgs('request.verb').returns("GET");
-    app.__set__('context', fakeContext);
-
-    var setVerb = app.__get__('setVerb');
-    expect(setVerb()).to.equal("GET");
-  });
-
-/*it('should fail a test on error', function(done) {
-		throw new Error('This test should fail!');
-	});
-
- it('should have POST', function() {
-    contextGetVariableMethod.withArgs('request.verb').returns("POST");
-    app.__set__('context', fakeContext);
-
-    var checkVerb = app.__get__('checkVerb');
-    expect(checkVerb()).to.equal("GET");
-  });*/
 });
